670f85b5fbde38df1dfbe5e3973548c5d872a138
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-05 10:08:18 +0700

    Completed Number 2

--- latihan-cots-1/2.sql
+++ latihan-cots-1/2.sql
@@ -1,2 +1,5 @@
 ALTER TABLE pegawai ADD COLUMN jenis_kelamin CHAR(1);
-ALTER TABLE pegawai ALTER COLUMN 
+ALTER TABLE pegawai MODIFY COLUMN nama VARCHAR(40);
+ALTER TABLE pegawai DROP COLUMN jenis_kelamin;
+ALTER TABLE order_has_barang RENAME COLUMN id_order TO id_transaksi; 
+ALTER TABLE pesanan ADD COLUMN total_bayar INT;
