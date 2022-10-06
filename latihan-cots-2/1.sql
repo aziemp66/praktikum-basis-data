1238984ae60b2705bf4e13ead105378bd1244002
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-06 09:20:38 +0700

    Latihan Cots 2

---
+++ latihan-cots-2/1.sql
@@ -0,0 +1,31 @@
+INSERT INTO pegawai VALUES 
+("PGW001","Mince","Desa Sukalenyap", "081234567890"),
+("PGW002", "Ngatiyem", "Desa Sukacita", "081345678912"),
+("PGW003", "Jaiman", "Desa Sukabakso", "081567890123"),
+("PGW004", "Markonah", "Desa Sukakamu", "081890123456"),
+("PGW005", "Raminten", "Desa Sukasukalodeh", "085678901234");
+
+INSERT INTO customer VALUES 
+("CUS001", "Lek Novo", "Ciamis", "080988999988"),
+("CUS002", "Sie Emen", "Cianjur", "080977888877"),
+("CUS003", "Erik Son", "Cibiru", "080966777766"),
+("CUS004", "Nu Kiue", "Cicindeu", "080955666655"),
+("CUS005", "Sam Soeng", "Ciamis", "080944555544"),
+("CUS006", "Philip", "Cicalengka", "080933444433"),
+("CUS007", "Va Iyo", "Cicalengka", "080922333322");
+
+INSERT INTO barang VALUES 
+("BRG001","Kulkas 8 pintu",9,200000)
+("BRG002","Pintu kemana saja",21,700000)
+("BRG004","Awan kinton",3,50000)
+("BRG005","Tongkat Sihir",107,2000)
+("BRG006","Sapu Terbang",86,350000)
+("BRG007","Sepatu Kaca",29,100000)
+("BRG008","Lampu Aladin",19,25000)
+("BRG009","Cobek Multifungsi",64,80000)
+("BRG010","Ramuan awet tua",31,250000)
+("BRG011","Bola kristal",2,1000)
+("BRG012","Sharingan",92,5000000)
+("BRG013","Kouhaku no Jouhei",19,800000)
+("BRG014","Samurai",77,1000000)
+("BRG015","Golok penebus nyawa",212,900000)
